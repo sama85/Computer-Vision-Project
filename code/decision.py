ea566dbf022fc8ff509079de812c46e521dbf576
hanyaAdel <hanya.elged@gmail.com>
2023-01-04 17:30:17 +0200

    renamed file

--- code/decision.py
+++ code/decision.py
@@ -3,7 +3,7 @@ Main module for decision handling
 
 """
 import states
-import transition_handlers
+import transition_actions
 
 
 """Handles switching between states"""
@@ -37,15 +37,15 @@ class DecisionMaker():
         if Rover.nav_angles is not None:
             # Mapping each handler to its corresponding state
             select = {
-                self.state[0]: transition_handlers.following_left_wall_transitions,
-                self.state[1]: transition_handlers.turning_to_left_wall_transitions,
-                self.state[2]: transition_handlers.avoiding_left_wall_transitions,
-                self.state[3]: transition_handlers.avoiding_obstacles_transitions,
-                self.state[4]: transition_handlers.going_to_sample_transitions,
-                self.state[5]: transition_handlers.stopping_at_sample_transitions,
-                self.state[6]: transition_handlers.getting_unstuck_transitions,
-                self.state[7]: transition_handlers.returning_home_transitions,
-                self.state[8]: transition_handlers.parking_at_home_transitions
+                self.state[0]: transition_actions.following_left_wall_transitions,
+                self.state[1]: transition_actions.turning_to_left_wall_transitions,
+                self.state[2]: transition_actions.avoiding_left_wall_transitions,
+                self.state[3]: transition_actions.avoiding_obstacles_transitions,
+                self.state[4]: transition_actions.going_to_sample_transitions,
+                self.state[5]: transition_actions.stopping_at_sample_transitions,
+                self.state[6]: transition_actions.getting_unstuck_transitions,
+                self.state[7]: transition_actions.returning_home_transitions,
+                self.state[8]: transition_actions.parking_at_home_transitions
             }
             func = select.get(self.curr_state)
 
